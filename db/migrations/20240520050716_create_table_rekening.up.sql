968163baab11e77532ef2f4d6bdb10c3922223d5
bilyanggaraa <bilyanggaraa@gmail.com>
2024-05-20 13:23:57 +0700

    [UPDATE] - updating typo on table rekening

--- db/migrations/20240520050716_create_table_rekening.up.sql
+++ db/migrations/20240520050716_create_table_rekening.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS rekening(
     user_id int NOT NULL,
     account_number varchar(50) NOT NULL,
     bank_name varchar(50) NOT NULL,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES roles(id)
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
 )
