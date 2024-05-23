c1eec43228607e5d7353c6e662dc7555021e2666
nugroho alif <nugroho.alif77@gmail.com>
2024-05-23 21:36:34 +0700

    [TEST] add unit test succes on all use case

--- src/debtCollector/debtCollectorUseCase/debtCollectorUseCase_test.go
+++ src/debtCollector/debtCollectorUseCase/debtCollectorUseCase_test.go
@@ -3,6 +3,7 @@ package debtCollectorUseCase
 import (
 	"errors"
 	"fp_pinjaman_online/model/dto/debtCollectorDto"
+	"fp_pinjaman_online/model/dto/json"
 	"fp_pinjaman_online/model/entity/debtCollectorEntity"
 	"fp_pinjaman_online/src/debtCollector"
 	"fp_pinjaman_online/src/debtCollector/debtCollectorRepository"
@@ -25,6 +26,40 @@ func (s *DebtCollectorUseCaseSuite) SetupTest() {
 
 func TestUsersUseCaseTestSuite(t *testing.T) {
 	suite.Run(t, new(DebtCollectorUseCaseSuite))
+}
+
+func (s *DebtCollectorUseCaseSuite) TestLogTugasAuthorizationCheck_Success() {
+	logTugasIdMock := "1"
+
+	expLog := debtCollectorEntity.LogTugas{}
+
+	s.dcRepoMock.Mock.On("SelectLogTugasById", logTugasIdMock).Return(expLog, nil)
+
+	log, err := s.usecase.LogTugasAuthorizationCheck(logTugasIdMock)
+
+	s.NoError(err)
+	s.Equal(expLog, log)
+}
+
+func (s *DebtCollectorUseCaseSuite) TestGetAllLateDebtorByCity_Success() {
+	dcIdMock := "1"
+	pageMock := 1
+	sizeMock := 5
+
+	expLoggedDc := debtCollectorEntity.DebtCollector{
+		City: "malang",
+	}
+	expLateDebtorList := []debtCollectorEntity.LateDebtor{}
+	expPagingMock := json.Paging{}
+
+	s.dcRepoMock.Mock.On("SelectDebtCollectorById", dcIdMock).Return(expLoggedDc, nil)
+	s.dcRepoMock.Mock.On("SelectAllLateDebitur", expLoggedDc.City, pageMock, sizeMock).Return(expLateDebtorList, expPagingMock, nil)
+
+	lateDebtorList, paging, err := s.usecase.GetAllLateDebtorByCity(dcIdMock, pageMock, sizeMock)
+
+	s.NoError(err)
+	s.Equal(expPagingMock, paging)
+	s.Equal(expLateDebtorList, lateDebtorList)
 }
 
 func (s *DebtCollectorUseCaseSuite) TestCreateLogTugas_Success() {
@@ -71,7 +106,7 @@ func (s *DebtCollectorUseCaseSuite) TestCreateLogTugas_Fail() {
 	})
 }
 
-func (s *DebtCollectorUseCaseSuite) TestGetLogTugasById() {
+func (s *DebtCollectorUseCaseSuite) TestGetLogTugasById_Success() {
 	mockLogTugasId := "1"
 	expectedLogTugas := debtCollectorEntity.LogTugas{
 		ID:          "1",
@@ -85,4 +120,167 @@ func (s *DebtCollectorUseCaseSuite) TestGetLogTugasById() {
 
 	s.NoError(err)
 	s.Equal(expectedLogTugas, log)
+}
+
+func (s *DebtCollectorUseCaseSuite) TestEditLogTugasById_Success() {
+	mockLogTugasId := "1"
+	expectedLogTugas := debtCollectorEntity.LogTugas{
+		ID:          "1",
+		TugasId:     "1",
+		Description: "tugas",
+	}
+	mockPayload := debtCollectorDto.UpdateLogTugasPayload{
+		Description: "tugas updated",
+	}
+
+	s.dcRepoMock.Mock.On("SelectLogTugasById", mockLogTugasId).Return(expectedLogTugas, nil)
+	s.dcRepoMock.Mock.On("UpdateLogTugasById", expectedLogTugas, mockPayload).Return(nil)
+
+	err := s.usecase.EditLogTugasById(mockLogTugasId, mockPayload)
+
+	s.NoError(err)
+}
+
+func (s *DebtCollectorUseCaseSuite) TestDeleteLogTugasById_Success() {
+	mockLogTugasId := "1"
+	expectedLogTugas := debtCollectorEntity.LogTugas{
+		ID:          "1",
+		TugasId:     "1",
+		Description: "tugas",
+	}
+
+	s.dcRepoMock.Mock.On("SelectLogTugasById", mockLogTugasId).Return(expectedLogTugas, nil)
+	s.dcRepoMock.Mock.On("SoftDeleteLogTugasById", mockLogTugasId).Return(nil)
+
+	err := s.usecase.DeleteLogTugasById(mockLogTugasId)
+
+	s.NoError(err)
+}
+
+func (s *DebtCollectorUseCaseSuite) TestGetAllLogTugas_Success() {
+	mockTugasId := "1"
+	expLogList := []debtCollectorEntity.LogTugas{{}}
+	expPaging := json.Paging{}
+
+	s.dcRepoMock.Mock.On("SelectAllLogByTugasId", mockTugasId, 0, 0).Return(expLogList, expPaging, nil)
+
+	logList, paging, err := s.usecase.GetAllLogTugas(mockTugasId, 0, 0)
+
+	s.NoError(err)
+	s.Equal(expLogList, logList)
+	s.Equal(expPaging, paging)
+}
+
+func (s *DebtCollectorUseCaseSuite) TestClaimTugas_Success() {
+	dcIdMock := "1"
+	mockPayload := debtCollectorDto.NewTugasPayload{
+		UserId: "1",
+	}
+	expLoggedDc := debtCollectorEntity.DebtCollector{
+		City: "malang",
+	}
+
+	s.dcRepoMock.Mock.On("CountOngoingTugas", dcIdMock).Return(2, nil)
+	s.dcRepoMock.Mock.On("SelectDebtCollectorById", dcIdMock).Return(expLoggedDc, nil)
+	s.dcRepoMock.Mock.On("SelectLateDebiturById", mockPayload.UserId, expLoggedDc.City).Return("", nil)
+	s.dcRepoMock.Mock.On("CreateClaimTugas", dcIdMock, mockPayload.UserId).Return(nil)
+
+	err := s.usecase.ClaimTugas(dcIdMock, mockPayload)
+
+	s.NoError(err)
+}
+
+func (s *DebtCollectorUseCaseSuite) TestClaimTugas_Fail() {
+	dcIdMock := "1"
+	mockPayload := debtCollectorDto.NewTugasPayload{
+		UserId: "1",
+	}
+	s.Run("Error on CountOngoingTugas", func() {
+		expCountError := errors.New("count error")
+		s.dcRepoMock.Mock.On("CountOngoingTugas", dcIdMock).Return(0, expCountError)
+
+		err := s.usecase.ClaimTugas(dcIdMock, mockPayload)
+
+		s.Error(err)
+		s.Equal(expCountError.Error(), err.Error())
+		s.dcRepoMock.Mock.AssertExpectations(s.T())
+		s.dcRepoMock.Mock.ExpectedCalls = nil
+	})
+
+	s.Run("Error max tugas exceed", func() {
+		expMaxError := errors.New("maximum ongoing tax is 3")
+		s.dcRepoMock.Mock.On("CountOngoingTugas", dcIdMock).Return(3, nil)
+
+		err := s.usecase.ClaimTugas(dcIdMock, mockPayload)
+
+		s.Error(err)
+		s.Equal(expMaxError.Error(), err.Error())
+		s.dcRepoMock.Mock.AssertExpectations(s.T())
+		s.dcRepoMock.Mock.ExpectedCalls = nil
+	})
+
+	s.Run("Error on SelectDebtCollectorById", func() {
+		expError := errors.New("dc not found")
+		s.dcRepoMock.Mock.On("CountOngoingTugas", dcIdMock).Return(2, nil)
+		s.dcRepoMock.Mock.On("SelectDebtCollectorById", dcIdMock).Return(debtCollectorEntity.DebtCollector{}, expError)
+
+		err := s.usecase.ClaimTugas(dcIdMock, mockPayload)
+
+		s.Error(err)
+		s.Equal(expError.Error(), err.Error())
+		s.dcRepoMock.Mock.AssertExpectations(s.T())
+		s.dcRepoMock.Mock.ExpectedCalls = nil
+	})
+
+	s.Run("Error on SelectLateDebiturById", func() {
+		expLoggedDc := debtCollectorEntity.DebtCollector{
+			City: "malang",
+		}
+		expError := errors.New("debitur not found")
+		s.dcRepoMock.Mock.On("CountOngoingTugas", dcIdMock).Return(2, nil)
+		s.dcRepoMock.Mock.On("SelectDebtCollectorById", dcIdMock).Return(expLoggedDc, nil)
+		s.dcRepoMock.Mock.On("SelectLateDebiturById", mockPayload.UserId, expLoggedDc.City).Return("", expError)
+
+		err := s.usecase.ClaimTugas(dcIdMock, mockPayload)
+
+		s.Error(err)
+		s.Equal(expError.Error(), err.Error())
+		s.dcRepoMock.Mock.AssertExpectations(s.T())
+		s.dcRepoMock.Mock.ExpectedCalls = nil
+	})
+
+	s.Run("Error on CreateClaimTugas", func() {
+		expLoggedDc := debtCollectorEntity.DebtCollector{
+			City: "malang",
+		}
+		expError := errors.New("error sql")
+		s.dcRepoMock.Mock.On("CountOngoingTugas", dcIdMock).Return(2, nil)
+		s.dcRepoMock.Mock.On("SelectDebtCollectorById", dcIdMock).Return(expLoggedDc, nil)
+		s.dcRepoMock.Mock.On("SelectLateDebiturById", mockPayload.UserId, expLoggedDc.City).Return("", nil)
+		s.dcRepoMock.Mock.On("CreateClaimTugas", dcIdMock, mockPayload.UserId).Return(expError)
+
+		err := s.usecase.ClaimTugas(dcIdMock, mockPayload)
+
+		s.Error(err)
+		s.Equal(expError.Error(), err.Error())
+		s.dcRepoMock.Mock.AssertExpectations(s.T())
+	})
+}
+
+func (s *DebtCollectorUseCaseSuite) TestGetAllTugas_Success() {
+	dcIdMock := "1"
+	statusMock := ""
+	pageMock := 1
+	sizeMock := 5
+
+	expListTugas := []debtCollectorEntity.Tugas{}
+	expPaging := json.Paging{}
+
+	s.dcRepoMock.Mock.On("SelectAllTugas", dcIdMock, statusMock, pageMock, sizeMock).Return(expListTugas, expPaging, nil)
+
+	listTugas, paging, err := s.usecase.GetAllTugas(dcIdMock, statusMock, pageMock, sizeMock)
+
+	s.NoError(err)
+	s.Equal(expPaging, paging)
+	s.Equal(expListTugas, listTugas)
 }
